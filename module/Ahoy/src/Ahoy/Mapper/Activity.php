f5bfd0bfd84d6d3ceb447e556afc35be8931381f
yonni <yonni.m@zend.com>
2013-09-12 16:37:08 +0300

    Activity mapper flexibility

--- module/Ahoy/src/Ahoy/Mapper/Activity.php
+++ module/Ahoy/src/Ahoy/Mapper/Activity.php
@@ -6,11 +6,19 @@ use Zend\Config\Reader;
 use Zend\Config\Writer;
 class Activity {
 	/**
+	 * @var string
+	 */
+	private $appPath;
+	
+	public function __construct() {
+		$this->appPath = getcwd();
+	}
+	/**
 	 * @return boolean
 	 */
 	public function isSiteActive() {
 		$reader = new Reader\Ini();
-		$config = $reader->fromFile(getcwd() . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'ahoy.ini');
+		$config = $reader->fromFile($this->appPath . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'ahoy.ini');
 		return (boolean)$config['ahoy']['active'];
 	}
 
@@ -21,7 +29,7 @@ class Activity {
 	public function setSiteActivity($active = true) {
 		$writer = new Writer\Ini();
 		$reader = new Reader\Ini();
-		$config = $reader->fromFile(getcwd() . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'ahoy.ini');
+		$config = $reader->fromFile($this->appPath . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'ahoy.ini');
 		$config['ahoy']['active'] = $active;
 		$writer->toFile(getcwd() . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'ahoy.ini', $config);
 		return $active;
