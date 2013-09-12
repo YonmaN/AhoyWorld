08341fab734dedab48a510fce9383fea29887750
yonni <yonni.m@zend.com>
2013-09-12 15:29:58 +0300

    Added isSiteActive functionality Added ini file to use for activity indicator

--- module/Ahoy/src/Ahoy/Mapper/Activity.php
+++ module/Ahoy/src/Ahoy/Mapper/Activity.php
@@ -2,12 +2,15 @@
 
 namespace Ahoy\Mapper;
 
+use Zend\Config\Reader\Ini;
 class Activity {
 	/**
 	 * @return boolean
 	 */
 	public function isSiteActive() {
-		return true;
+		$reader = new Ini();
+		$config = $reader->fromFile(getcwd() . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'ahoy.ini');
+		return (boolean)$config['ahoy']['active'];
 	}
 	
 	public function setSiteActivity($active = true) {
